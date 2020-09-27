e3c341ac06cd35ece7fbd50976b71df8b75d59e5
nawan44 <rachmat.d.gunawan.com>
2020-09-27 21:46:13 +0700

    navmenu

--- src/component/navmenu.jsx
+++ src/component/navmenu.jsx
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Collapse, Navbar, NavbarToggler,
+  NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown,
+  DropdownToggle, DropdownMenu, DropdownItem, NavbarText
+} from 'reactstrap';
 import './css/navmenu.css';
 
-class Navmenu extends React.Component{
-render(){
-    return(
-        <div>
-            Navmenu
-        </div>
-    );
+const Navmenu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="navmen" >
+      <Navbar  color="light" light expand="md">
+        <NavbarBrand href="/" className="logo">Rahma Kitchen</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Bento</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Cara Pemesanan</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Kue
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Kue Kering
+                </DropdownItem>
+                <DropdownItem>
+                  Kue Basah
+                </DropdownItem>
+
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Paket
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                  Bento
+                </DropdownItem>
+                <DropdownItem>
+                  Snack Box
+                </DropdownItem>
+                <DropdownItem>
+                  Kue Pernikahan
+                </DropdownItem>
+
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="#">Tips Trik</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Contact</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
-}
+
 export default Navmenu;
