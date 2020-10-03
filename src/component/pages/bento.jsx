423ce4bc2cb97b1519a89f08b4e260ced58d467d
nawan44 <rachmat.d.gunawan.com>
2020-10-04 00:17:11 +0700

    pages

---
+++ src/component/pages/bento.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Bento extends React.Component{
+    render(){
+        return(
+           <div>
+               
+           </div>
+        );
+    }
+}
+
+export default Bento
---
+++ src/component/pages/kueKering.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class KueKering extends React.Component{
+    render(){
+        return(
+           <div>
+               
+           </div>
+        );
+    }
+}
+
+export default KueKering
